c8a46e7ec09f829df8b0eabc722b460e462a90f7
Prathamesh Kokate <prathamesh.kokate@flyingcursor.com>
2022-09-19 10:35:55 +0530

    Started with postman

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 2000
+
+const app = express();
+
+app.listen(port, ()=> console.log(`server running on: ${port}`));
